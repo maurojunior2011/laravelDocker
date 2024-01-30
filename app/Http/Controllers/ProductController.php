f6a971e477edc9d797de2d5be84e78d1a0369a91
Mauro Júnior <maurojunior2011@gmail.com>
2024-01-30 11:54:27 -0300

    Configuração básica do Swagger: /api/documentation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,62 +5,36 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
+use Illuminate\Http\JsonResponse;
 
 class ProductController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+     * @OA\Get(
+     *     tags={"All Products"},
+     *     summary="Returns a list of products",
+     *     description="Returns a array of products",
+     *     path="/api/v1/allproducts",
+     *     @OA\Response(response="200", description="A list with/without products"),
+     *     @OA\Response(response="400", description="Error"),
+     * ),
+     * 
+    */
+    public function allProducts() {
+        $errormessages = []; //to possible validation
+        $warningmessages = []; //to possible validation
+        $error = false; //to possible validation
+        $warning = false; //to possible validation
+        $values = Product::get(['id','name','price', 'description']);
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreProductRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateProductRequest $request, Product $product)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Product $product)
-    {
-        //
+        return new JsonResponse(['data' => [
+            'values' => $values,
+            'success' => ($error) ? false : true,
+            'message' => $error ? 'Erro ao buscar os dados!' : ($warning ? 'Sucesso com avisos!' : 'Sucesso!'),
+            'errors' => count($errormessages),
+            'errormessages' => $errormessages,
+            'warnings' => count($warningmessages),
+            'warningmessages' => $warningmessages
+        ]], $error ? 400 : 200);        
     }
 }
